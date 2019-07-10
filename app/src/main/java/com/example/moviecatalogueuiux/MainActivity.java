4fe9deec1d39efc6cde0e971a79d12d190366382
miftahfarhan13 <miftahfarhan20@gmail.com>
2019-07-10 09:02:43 +0700

    fitur ubah bahasa commit

--- app/src/main/java/com/example/moviecatalogueuiux/MainActivity.java
+++ app/src/main/java/com/example/moviecatalogueuiux/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.moviecatalogueuiux;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
@@ -8,7 +10,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.annotation.NonNull;
 import android.view.MenuItem;
 import android.widget.TextView;
-
+import android.view.Menu;
 import com.example.moviecatalogueuiux.Fragment.MovieFragment;
 import com.example.moviecatalogueuiux.Fragment.TvShowFragment;
 
@@ -45,6 +47,22 @@ public class MainActivity extends AppCompatActivity {
         Fragment fragment;
         fragment = new MovieFragment();
         loadFragment(fragment);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_change_settings) {
+            Intent mIntent = new Intent(Settings.ACTION_LOCALE_SETTINGS);
+            startActivity(mIntent);
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private void loadFragment(Fragment fragment) {
